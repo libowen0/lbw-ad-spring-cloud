dcf6711fff44dc57f16e1ab4240b1c7a1867ea11
libowen <libw@nroad.com.cn>
2019-02-21 17:50:24 +0800

    add multi-node eureka server

---
+++ ad-eureka/src/main/java/com/lbw/ad/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.lbw.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created with IntelliJ IDEA. User: lbw Date: 2019/2/21 Time: 11:09 Description:
+ */
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
