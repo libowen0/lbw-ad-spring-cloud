ae07abdc42006858d64e2d2324322a9d2dbe43ee
libowen <libw@nroad.com.cn>
2019-02-22 11:08:54 +0800

    update gitignore

---
+++ lbw-ad-service/ad-common/src/main/java/com/lbw/ad/advice/CommonResponseDataAdvice.java
@@ -0,0 +1,7 @@
+package com.lbw.ad.advice;
+
+/**
+ * Created with IntelliJ IDEA. User: lbw Date: 2019/2/22 Time: 10:42 Description:
+ */
+public class CommonResponseDataAdvice {
+}
---
+++ lbw-ad-service/ad-common/src/main/java/com/lbw/ad/vo/CommonResponse.java
@@ -0,0 +1,7 @@
+package com.lbw.ad.vo;
+
+/**
+ * Created with IntelliJ IDEA. User: lbw Date: 2019/2/22 Time: 10:27 Description:
+ */
+public class CommonResponse {
+}
